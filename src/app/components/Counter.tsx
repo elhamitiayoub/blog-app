4ffc41a562aa2e1717f8a529c8a4d37783788392
elhamitiayoub <elhamitiayoub5@gmail.com>
2025-09-20 20:10:55 +0100

    added components and updated project structure

--- src/app/components/Counter.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useState } from "react";
-
-
-export function Counter() {
-    const[count,setCount] = useState(0);
-
-    return <button onClick={() => setCount(count+1)}>count: {count}</button>
-}
--- src/app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div>
-      <h1>Hello From the Dashboard page</h1>
-      
-    </div>
-  );
-}
